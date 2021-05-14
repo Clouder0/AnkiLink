0beebd437d967ccefe508634021b3922999ccd34
Clouder0 <clouder0@outlook.com>
2021-05-14 23:54:27 +0800

    added Linux distribution

--- test.bash
+++
@@ -1,5 +0,0 @@
-python src/AnkiImporter.py tests/test1.md
-python src/AnkiImporter.py tests/test2.md
-python src/AnkiImporter.py tests/test3.md
-python src/AnkiImporter.py tests/test4.md
-python src/AnkiImporter.py tests/test5.md
