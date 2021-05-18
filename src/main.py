f5d4c29fcda2c6fbc2b689667f803ee6f5dce31b
feiko <pan-fei@outlook.com>
2021-05-18 19:42:08 +0800

    bug fix

--- src/main.py
+++ src/main.py
@@ -1,7 +1,5 @@
-import os
 import sys
 import datetime
-import markdown2
 from NoteType import QA, Choices, Cloze
 
  
@@ -30,20 +28,11 @@ def HandlePost(text):
         f.write(HandleNote(note))
     f.close()
 
-def check_library(lib_name, auto_install = True):
-    if lib_name not in os.popen('python -m pip list').read():
-        print('The required module \'{}\' has not installed yet.'.format(lib_name))
-        if auto_install:
-            print('It is being installed automatically:')
-            os.system('python -m pip install {}'.format(lib_name))
-    exec('import {}'.format(lib_name))
-
 if len(sys.argv) < 2: 
     print("Please provide a param: python AnkiImporter.py filename.md")
     sys.exit()
 path = sys.argv[1]
 print("path: " + path)
-check_library('markdown2')
 f = open(path,"r",encoding = "utf-8")
 HandlePost(f.read())
 f.close()
